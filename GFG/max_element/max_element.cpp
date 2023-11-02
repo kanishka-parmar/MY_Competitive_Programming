e763f1b51b077b19423aa67eb021e847705aa764
kanishka-parmar <kanishkaparmar2004@gmail.com>
2023-11-02 23:56:58 +0530

    Largest element

---
+++ GFG/max_element/max_element.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    { int max=0;
+       for(int i=0;i<n;i++){
+           if(arr[i]>max){
+               max=arr[i];
+           }
+       }
+       return max;
+        
+    }
+};
